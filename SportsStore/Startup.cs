c46d92600a701e4f50a396942f2c2fff91a06cdf
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 12:40:02 +0100

    add seed data to startup pipeline

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -1,14 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SportsStore.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.DependencyInjection;
-using SportsStore.Models;
-using Microsoft.Extensions.Configuration;
-using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
@@ -25,8 +25,8 @@ namespace SportsStore
     {
       services
         .AddDbContext<ApplicationDbContext>(options =>
-                                                  options.UseNpgsql(
-                                                    Configuration["Data:SportsStoreProducts:ConnectionString"]));
+                                            options.UseNpgsql(
+                                              Configuration["Data:SportsStoreProducts:ConnectionString"]));
       services.AddTransient<IProductRepository, EFProductRepository>();
       services.AddMvc();
     }
@@ -47,6 +47,7 @@ namespace SportsStore
           template: "{controller=Product}/{action=List}/{id?}"
         );
       });
+      SeedData.EnsurePopulated(app);
     }
   }
 }
