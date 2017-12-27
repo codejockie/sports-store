7f79adc3b034e048109e9084e38beaa8d5fa72ab
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 10:51:10 +0100

    create product controller

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+  public class ProductController : Controller
+  {
+    IProductRepository repository;
+
+    public ProductController(IProductRepository repo)
+    {
+      repository = repo;
+    }
+
+    public IActionResult List() => View(repository.Products);
+  }
+}
