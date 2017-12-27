97c3e20d6545ecde8484e6fe287a8cc4729e1fd1
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 22:44:38 +0100

    add class support to page link tag helper style app

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -25,6 +25,11 @@ namespace SportsStore.Infrastructure
 
     public string PageAction { get; set; }
 
+    public bool PageClassesEnabled { get; set; } = false;
+    public string PageClass { get; set; }
+    public string PageClassNormal { get; set; }
+    public string PageClassSelected { get; set; }
+
     public override void Process(TagHelperContext context,
                                 TagHelperOutput output)
     {
@@ -35,6 +40,13 @@ namespace SportsStore.Infrastructure
         TagBuilder tag = new TagBuilder("a");
         tag.Attributes["href"] = urlhelper.Action(PageAction,
                                                   new { productPage = i });
+
+        if (PageClassesEnabled)
+        {
+          tag.AddCssClass(PageClass);
+          tag.AddCssClass(i == PageModel.CurrentPage ?
+                          PageClassSelected : PageClassNormal);
+        }
         tag.InnerHtml.Append(i.ToString());
         result.InnerHtml.AppendHtml(tag);
       }
