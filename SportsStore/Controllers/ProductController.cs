0441bc0d3d6269e88b1a783775fcf5864a4c71de
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 12:48:06 +0100

    add pagination to product list

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,17 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
   public class ProductController : Controller
   {
     IProductRepository repository;
+    public int PageSize = 4;
 
     public ProductController(IProductRepository repo)
     {
       repository = repo;
     }
 
-    public IActionResult List() => View(repository.Products);
+    public IActionResult List(int productPage = 1) =>
+      View(repository.Products
+           .OrderBy(p => p.ProductID)
+           .Skip((productPage - 1) * PageSize)
+           .Take(PageSize));
   }
 }
