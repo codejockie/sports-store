fb7405eb50770b7accb154bbfe8f5c0c255ab6c8
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 13:31:52 +0100

    create page link tag helper

--- SportsStore/Infrastructure/PageLinkTagHelper.cs
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -1,10 +1,44 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using SportsStore.Models.ViewModels;
+
 namespace SportsStore.Infrastructure
 {
-  public class PageLinkTagHelper
+  [HtmlTargetElement("div", Attributes = "page-model")]
+  public class PageLinkTagHelper : TagHelper
   {
-    public PageLinkTagHelper()
+    IUrlHelperFactory urlHelperFactory;
+
+    public PageLinkTagHelper(IUrlHelperFactory helperFactory)
     {
+      urlHelperFactory = helperFactory;
+    }
+
+    [ViewContext]
+    [HtmlAttributeNotBound]
+    public ViewContext ViewContext { get; set; }
+
+    public PagingInfo PageModel { get; set; }
+
+    public string PageAction { get; set; }
+
+    public override void Process(TagHelperContext context,
+                                TagHelperOutput output)
+    {
+      IUrlHelper urlhelper = urlHelperFactory.GetUrlHelper(ViewContext);
+      TagBuilder result = new TagBuilder("div");
+      for (int i = 1; i <= PageModel.TotalPages; i++)
+      {
+        TagBuilder tag = new TagBuilder("a");
+        tag.Attributes["href"] = urlhelper.Action(PageAction,
+                                                  new { productPage = i });
+        tag.InnerHtml.Append(i.ToString());
+        result.InnerHtml.AppendHtml(tag);
+      }
+      output.Content.AppendHtml(result.InnerHtml);
     }
   }
 }
