cfbb8c38fcb69ceb8101ef5b89ff5ff9ea905767
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 12:27:59 +0100

    configure app to read connection string with Npgsql replace fake repo with ef repo disable scope verification

--- SportsStore/Startup.cs
+++ SportsStore/Startup.cs
@@ -7,16 +7,27 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using SportsStore.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
 
 namespace SportsStore
 {
   public class Startup
   {
+    public Startup(IConfiguration configuration) =>
+      Configuration = configuration;
+
+    public IConfiguration Configuration { get; }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddTransient<IProductRepository, FakeProductRepository>();
+      services
+        .AddDbContext<ApplicationDbContext>(options =>
+                                                  options.UseNpgsql(
+                                                    Configuration["Data:SportsStoreProducts:ConnectionString"]));
+      services.AddTransient<IProductRepository, EFProductRepository>();
       services.AddMvc();
     }
 
