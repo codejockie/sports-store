9db16cd90943db7ac0c3f40d5c0877fc0ccc0fed
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 17:39:45 +0100

    pass product list view model to list action

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using SportsStore.Models.ViewModels;
 using System.Linq;
 
 namespace SportsStore.Controllers
@@ -15,9 +16,18 @@ namespace SportsStore.Controllers
     }
 
     public ViewResult List(int productPage = 1) =>
-      View(repository.Products
-           .OrderBy(p => p.ProductID)
-           .Skip((productPage - 1) * PageSize)
-           .Take(PageSize));
+      View(new ProductsListViewModel
+      {
+        Products = repository.Products
+                             .OrderBy(p => p.ProductID)
+                             .Skip((productPage - 1) * PageSize)
+                             .Take(PageSize),
+        PagingInfo = new PagingInfo
+        {
+          CurrentPage = productPage,
+          ItemsPerPage = PageSize,
+          TotalItems = repository.Products.Count()
+        }
+      });
   }
 }
