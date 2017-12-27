c652c5cac928e5fc437e4eeccf3afe0cdcfefdd5
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-27 17:39:15 +0100

    create product list view model

---
+++ SportsStore/Models/ViewModels/ProductsListViewModel.cs
@@ -0,0 +1,11 @@
+﻿using SportsStore.Models;
+using System.Collections.Generic;
+
+namespace SportsStore.Models.ViewModels
+{
+  public class ProductsListViewModel
+  {
+    public IEnumerable<Product> Products { get; set; }
+    public PagingInfo PagingInfo { get; set; }
+  }
+}
