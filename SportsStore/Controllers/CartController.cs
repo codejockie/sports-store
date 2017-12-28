716020b9e01017a2775d9c1a5bd3bf5a3d595e7f
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-28 12:32:39 +0100

    implement cart controller

---
+++ SportsStore/Controllers/CartController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SportsStore.Infrastructure;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+  public class CartController : Controller
+  {
+    readonly IProductRepository repository;
+
+    public CartController(IProductRepository repo)
+    {
+      repository = repo;
+    }
+
+    public RedirectToActionResult AddToCart(int productId, string returnUrl)
+    {
+      Product product = repository.Products
+                                  .FirstOrDefault(p => p.ProductID == productId);
+
+      if (product != null)
+      {
+        Cart cart = GetCart();
+        cart.AddItem(product, 1);
+        SaveCart(cart);
+      }
+
+      return RedirectToAction("Index", new { returnUrl });
+    }
+
+    public RedirectToActionResult RemoveFromCart(int productId, string returnUrl)
+    {
+      Product product = repository.Products
+                                  .FirstOrDefault(p => p.ProductID == productId);
+
+      if (product != null)
+      {
+        Cart cart = GetCart();
+        cart.RemoveLine(product);
+        SaveCart(cart);
+      }
+      return RedirectToAction("Index", new { returnUrl });
+    }
+
+    Cart GetCart()
+    {
+      Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
+      return cart;
+    }
+
+    void SaveCart(Cart cart)
+    {
+      HttpContext.Session.SetJson("Cart", cart);
+    }
+  }
+}
---
+++ SportsStore/Infrastructure/SessionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+
+namespace SportsStore.Infrastructure
+{
+  // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+  public class SessionExtensions
+  {
+    private readonly RequestDelegate _next;
+
+    public SessionExtensions(RequestDelegate next)
+    {
+      _next = next;
+    }
+
+    public Task Invoke(HttpContext httpContext)
+    {
+
+      return _next(httpContext);
+    }
+  }
+
+  // Extension method used to add the middleware to the HTTP request pipeline.
+  public static class SessionExtensionsExtensions
+  {
+    public static IApplicationBuilder UseMiddlewareClassTemplate(this IApplicationBuilder builder)
+    {
+      return builder.UseMiddleware<SessionExtensions>();
+    }
+  }
+}
