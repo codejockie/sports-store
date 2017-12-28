a557651a0e7c1ef9a1d280f109ad8855baf166cf
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-28 09:38:50 +0100

    test navigation menu view component

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Moq;
+using SportsStore.Components;
+using SportsStore.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
 namespace SportsStore.Tests
 {
   public class NavigationMenuViewComponentTests
   {
-    public NavigationMenuViewComponentTests()
+    [Fact]
+    public void Can_Select_Categories()
     {
+      // Arrange
+      var mock = new Mock<IProductRepository>();
+      mock.Setup(m => m.Products).Returns((new Product[] {
+        new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+        new Product {ProductID = 2, Name = "P2", Category = "Apples"},
+        new Product {ProductID = 3, Name = "P3", Category = "Plums"},
+        new Product {ProductID = 4, Name = "P4", Category = "Oranges"},
+      }).AsQueryable<Product>());
+
+      NavigationMenuViewComponent target =
+        new NavigationMenuViewComponent(mock.Object);
+
+      // Act = get the set of categories
+      string[] results = ((IEnumerable<string>)(target.Invoke()
+                                                as ViewViewComponentResult)
+                          .ViewData.Model).ToArray();
+
+      // Assert
+      Assert.True(Enumerable.SequenceEqual(new string[] {
+        "Apples", "Oranges", "Plums"
+      }, results));
     }
   }
 }
