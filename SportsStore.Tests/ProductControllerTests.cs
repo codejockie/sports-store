0df63b9e057964c51a4c94b54ce8d1dec2181efe
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-28 10:32:11 +0100

    test total page count fix

--- SportsStore.Tests/ProductControllerTests.cs
+++ SportsStore.Tests/ProductControllerTests.cs
@@ -1,7 +1,9 @@
-﻿using Moq;
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
 using SportsStore.Controllers;
 using SportsStore.Models;
 using SportsStore.Models.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using Xunit;
@@ -91,6 +93,37 @@ namespace SportsStore.Tests
       Assert.Equal(2, result.Length);
       Assert.True(result[0].Name == "P2" && result[0].Category == "Cat2");
       Assert.True(result[1].Name == "P4" && result[1].Category == "Cat2");
+    }
+
+    [Fact]
+    public void Generate_Category_Specific_Product_Count()
+    {
+      // Arrange
+      Mock<IProductRepository> mock = new Mock<IProductRepository>();
+      mock.Setup(m => m.Products).Returns((new Product[] {
+        new Product {ProductID = 1, Name = "P1", Category = "Cat1"},
+        new Product {ProductID = 2, Name = "P2", Category = "Cat2"},
+        new Product {ProductID = 3, Name = "P3", Category = "Cat1"},
+        new Product {ProductID = 4, Name = "P4", Category = "Cat2"},
+        new Product {ProductID = 5, Name = "P5", Category = "Cat3"}
+      }).AsQueryable<Product>());
+
+      ProductController target = new ProductController(mock.Object);
+      target.PageSize = 3;
+      Func<ViewResult, ProductsListViewModel> GetModel = result =>
+      result?.ViewData?.Model as ProductsListViewModel;
+      
+      // Action
+      int? res1 = GetModel(target.List("Cat1"))?.PagingInfo.TotalItems;
+      int? res2 = GetModel(target.List("Cat2"))?.PagingInfo.TotalItems;
+      int? res3 = GetModel(target.List("Cat3"))?.PagingInfo.TotalItems;
+      int? resAll = GetModel(target.List(null))?.PagingInfo.TotalItems;
+
+      // Assert
+      Assert.Equal(2, res1);
+      Assert.Equal(2, res2);
+      Assert.Equal(1, res3);
+      Assert.Equal(5, resAll);
     }
   }
 }
