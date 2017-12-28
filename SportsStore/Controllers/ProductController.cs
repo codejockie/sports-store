b9cd39a92ed673c69ab1a63074ae1695e83a727b
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-28 08:41:43 +0100

    add category filter to action

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -15,10 +15,11 @@ namespace SportsStore.Controllers
       repository = repo;
     }
 
-    public ViewResult List(int productPage = 1) =>
+    public ViewResult List(string category, int productPage = 1) =>
       View(new ProductsListViewModel
       {
         Products = repository.Products
+                             .Where(p => category == null || p.Category == category)
                              .OrderBy(p => p.ProductID)
                              .Skip((productPage - 1) * PageSize)
                              .Take(PageSize),
@@ -27,7 +28,8 @@ namespace SportsStore.Controllers
           CurrentPage = productPage,
           ItemsPerPage = PageSize,
           TotalItems = repository.Products.Count()
-        }
+        },
+        CurrentCategory = category
       });
   }
 }
