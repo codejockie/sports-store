21dfafa2679737a98988bbb56fb70e24dc5ecb78
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-28 10:13:08 +0100

    test for currently selected category

--- SportsStore.Tests/NavigationMenuViewComponentTests.cs
+++ SportsStore.Tests/NavigationMenuViewComponentTests.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Routing;
 using Moq;
 using SportsStore.Components;
 using SportsStore.Models;
@@ -34,6 +36,38 @@ namespace SportsStore.Tests
       Assert.True(Enumerable.SequenceEqual(new string[] {
         "Apples", "Oranges", "Plums"
       }, results));
+    }
+
+    [Fact]
+    public void Indicates_Selected_Category()
+    {
+      // Arrange
+      string categoryToSelect = "Apples";
+      var mock = new Mock<IProductRepository>();
+      mock.Setup(m => m.Products).Returns((new Product[] {
+        new Product {ProductID = 1, Name = "P1", Category = "Apples"},
+        new Product {ProductID = 4, Name = "P2", Category = "Oranges"},
+      }).AsQueryable<Product>());
+
+      var target = new NavigationMenuViewComponent(mock.Object)
+      {
+        ViewComponentContext = new ViewComponentContext
+        {
+          ViewContext = new ViewContext
+          {
+            RouteData = new RouteData()
+          }
+        }
+      };
+      target.RouteData.Values["category"] = categoryToSelect;
+
+      // Action
+      var result = (string)(target.Invoke() as
+                            ViewViewComponentResult)
+        .ViewData["SelectedCategory"];
+
+      // Assert
+      Assert.Equal(categoryToSelect, result);
     }
   }
 }
