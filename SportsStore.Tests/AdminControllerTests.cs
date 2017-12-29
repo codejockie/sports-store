2580491551e1e7df93ea6f32a6c2377a3660ceb3
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-29 17:57:07 +0100

    create admin controller with index action

---
+++ SportsStore.Tests/AdminControllerTests.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace SportsStore.Tests
+{
+  public class AdminControllerTests
+  {
+    public AdminControllerTests()
+    {
+    }
+  }
+}
---
+++ SportsStore/Controllers/AdminController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+  public class AdminController : Controller
+  {
+    readonly IProductRepository repository;
+
+    public AdminController(IProductRepository repo)
+    {
+      repository = repo;
+    }
+
+    public ViewResult Index() => View(repository.Products);
+  }
+}
