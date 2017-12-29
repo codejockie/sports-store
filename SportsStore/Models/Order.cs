9a3e483449d3e163ae917029ebb761210541ec98
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-29 17:13:57 +0100

    add shipped property to order model

--- SportsStore/Models/Order.cs
+++ SportsStore/Models/Order.cs
@@ -11,6 +11,9 @@ namespace SportsStore.Models
     [BindNever]
     public ICollection<CartLine> Lines { get; set; }
 
+    [BindNever]
+    public bool Shipped { get; set; }
+
     [Required(ErrorMessage = "Please enter a name")]
     public string Name { get; set; }
 
