64380477a2a14fab0461f516d090537efda55280
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-29 19:02:52 +0100

    implement admin edit action

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SportsStore.Models;
+using System.Linq;
 
 namespace SportsStore.Controllers
 {
@@ -13,5 +14,9 @@ namespace SportsStore.Controllers
     }
 
     public ViewResult Index() => View(repository.Products);
+
+    public ViewResult Edit(int productId) =>
+      View(repository.Products
+          .FirstOrDefault(p => p.ProductID == productId));
   }
 }
