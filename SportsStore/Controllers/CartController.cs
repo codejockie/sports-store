f526d8d86aac0f2a726fe453bef1dd46c21dd13a
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-29 10:27:05 +0100

    simply cart controller

--- SportsStore/Controllers/CartController.cs
+++ SportsStore/Controllers/CartController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using System.Linq;
-using SportsStore.Infrastructure;
 using SportsStore.Models;
 using SportsStore.Models.ViewModels;
 
@@ -10,17 +8,19 @@ namespace SportsStore.Controllers
   public class CartController : Controller
   {
     readonly IProductRepository repository;
+    readonly Cart cart;
 
-    public CartController(IProductRepository repo)
+    public CartController(IProductRepository repo, Cart cartService)
     {
       repository = repo;
+      cart = cartService;
     }
 
     public ViewResult Index(string returnUrl)
     {
       return View(new CartIndexViewModel
       {
-        Cart = GetCart(),
+        Cart = cart,
         ReturnUrl = returnUrl
       });
     }
@@ -32,9 +32,7 @@ namespace SportsStore.Controllers
 
       if (product != null)
       {
-        Cart cart = GetCart();
         cart.AddItem(product, 1);
-        SaveCart(cart);
       }
 
       return RedirectToAction("Index", new { returnUrl });
@@ -47,22 +45,9 @@ namespace SportsStore.Controllers
 
       if (product != null)
       {
-        Cart cart = GetCart();
         cart.RemoveLine(product);
-        SaveCart(cart);
       }
       return RedirectToAction("Index", new { returnUrl });
-    }
-
-    Cart GetCart()
-    {
-      Cart cart = HttpContext.Session.GetJson<Cart>("Cart") ?? new Cart();
-      return cart;
-    }
-
-    void SaveCart(Cart cart)
-    {
-      HttpContext.Session.SetJson("Cart", cart);
     }
   }
 }
