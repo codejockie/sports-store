97ca183f52a9ab781f2ad5bc80e492fa0c1beec1
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-29 20:41:39 +0100

    implement admin delete product action

--- SportsStore/Controllers/AdminController.cs
+++ SportsStore/Controllers/AdminController.cs
@@ -33,5 +33,16 @@ namespace SportsStore.Controllers
     }
 
     public ViewResult Create() => View("Edit", new Product());
+
+    [HttpPost]
+    public IActionResult Delete(int productId)
+    {
+      Product deletedProduct = repository.DeleteProduct(productId);
+      if (deletedProduct != null)
+      {
+        TempData["message"] = $"{deletedProduct.Name} was deleted";
+      }
+      return RedirectToAction("Index");
+    }
   }
 }
