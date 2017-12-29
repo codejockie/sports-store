826b6d118eeaa9c7792344594cc9e075f6e07e4f
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-29 15:26:43 +0100

    implement order repo interface

--- SportsStore/Models/EFOrderRepository.cs
+++ SportsStore/Models/EFOrderRepository.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
 namespace SportsStore.Models
 {
-  public class EFOrderRepository
+  public class EFOrderRepository : IOrderRepository
   {
-    public EFOrderRepository()
+    readonly ApplicationDbContext context;
+
+    public EFOrderRepository(ApplicationDbContext ctx)
     {
+      context = ctx;
+    }
+
+    public IQueryable<Order> Orders => context.Orders
+                                              .Include(o => o.Lines)
+                                              .ThenInclude(l => l.Product);
+
+    public void SaveOrder(Order order)
+    {
+      // prevents Product object from been written if it already exists
+      // prevents EF Core from writing the deserialised Product objects that
+      // are associated with the Order object.
+      context.AttachRange(order.Lines.Select(l => l.Product));
+      if (order.OrderID == 0)
+      {
+        context.Orders.Add(order);
+      }
+      context.SaveChanges();
     }
   }
 }
