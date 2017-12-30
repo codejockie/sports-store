0676a7195d539ce5020399d16c376b9fb98b0053
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-30 14:23:09 +0100

    implement account controller

--- SportsStore/Controllers/AccountController.cs
+++ SportsStore/Controllers/AccountController.cs
@@ -1,19 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using SportsStore.Models.ViewModels;
+using System.Threading.Tasks;
 
 namespace SportsStore.Controllers
 {
+  [Authorize]
   public class AccountController : Controller
   {
-    // GET: /<controller>/
-    public IActionResult Index()
+    readonly UserManager<IdentityUser> userManager;
+    readonly SignInManager<IdentityUser> signInManager;
+
+    public AccountController(UserManager<IdentityUser> userMgr,
+                             SignInManager<IdentityUser> signInMgr)
     {
-      return View();
+      userManager = userMgr;
+      signInManager = signInMgr;
+    }
+
+    [AllowAnonymous]
+    public ViewResult Login(string returnUrl)
+    {
+      return View(new LoginModel
+      {
+        ReturnUrl = returnUrl
+      });
+    }
+
+    [HttpPost]
+    [AllowAnonymous]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(LoginModel loginModel)
+    {
+      if (ModelState.IsValid)
+      {
+        IdentityUser user =
+          await userManager.FindByNameAsync(loginModel.Name);
+        if (user != null)
+        {
+          await signInManager.SignOutAsync();
+          if ((await signInManager.PasswordSignInAsync(user,
+                                                       loginModel.Password,
+                                                       false, false)).Succeeded)
+          {
+            return Redirect(loginModel?.ReturnUrl ?? "/admin/index");
+          }
+        }
+      }
+      ModelState.AddModelError("", "Invalid name or password");
+      return View(loginModel);
+    }
+
+    public async Task<RedirectResult> Logout(string returnUrl = "/")
+    {
+      await signInManager.SignOutAsync();
+      return Redirect(returnUrl);
     }
   }
 }
