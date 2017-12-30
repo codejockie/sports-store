57348581f66d7e81a9f7bfccb595fa8897a7da8c
John Kennedy (codejockie) <devjckennedy@gmail.com>
2017-12-30 13:31:08 +0100

    create identity seed data

--- SportsStore/Models/IdentitySeedData.cs
+++ SportsStore/Models/IdentitySeedData.cs
@@ -1,10 +1,25 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace SportsStore.Models
 {
-  public class IdentitySeedData
+  public static class IdentitySeedData
   {
-    public IdentitySeedData()
+    const string adminUser = "Admin";
+    const string adminPassword = "Secret123$";
+
+    public static async void EnsurePopulated(IApplicationBuilder app)
     {
+      UserManager<IdentityUser> userManager = app.ApplicationServices
+                                                 .GetRequiredService<UserManager<IdentityUser>>();
+
+      IdentityUser user = await userManager.FindByIdAsync(adminUser);
+      if (user == null)
+      {
+        user = new IdentityUser("Admin");
+        await userManager.CreateAsync(user, adminPassword);
+      }
     }
   }
 }
